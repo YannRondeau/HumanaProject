e8cbe4553ac2882470532827815730827c70c028
YannRondeau <rondeau.yann@hotmail.fr>
2019-09-24 17:32:48 +0200

    projet3

--- bootstrap.php
+++ bootstrap.php
@@ -2,3 +2,10 @@
 session_start();
 include 'services/tools.php';
 include 'services/server.php';
+
+
+foreach ($_POST as $key => $value) {
+    $_POST[$key] = htmlspecialchars($value, ENT_QUOTES,"UTF-8");
+}
+
+?>
